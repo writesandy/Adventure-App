449cdcd0ba03e56eca3314055bcc633f7014b4fd
writesandy <writesandy@gmail.com>
2018-04-04 20:34:34 -0500

    new button with input from DoM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,44 @@
-"use strict";
+$(document).ready(function() {
 
 //Initialize RapidAPI (the middleman to Spoonacular API)
 //var RapidAPI = new require('rapidapi-connect');
 //var rapid = new RapidAPI('default-application_5abfa693e4b00687d3579d2a', '35797c0f-67c6-4386-9991-a3a8c18e340f');
 
 // Initialize the Firebase connection
-var config = {
-    apiKey: "AIzaSyDmoyIPkRW6mPdQOSnj0uXIOipmiU_1gc4",
-    authDomain: "recipe-app-52f20.firebaseapp.com",
-    databaseURL: "https://recipe-app-52f20.firebaseio.com",
-    projectId: "recipe-app-52f20",
-    storageBucket: "recipe-app-52f20.appspot.com",
-    messagingSenderId: "1016364414039"
-  };
+// var config = {
+//     apiKey: "AIzaSyDmoyIPkRW6mPdQOSnj0uXIOipmiU_1gc4",
+//     authDomain: "recipe-app-52f20.firebaseapp.com",
+//     databaseURL: "https://recipe-app-52f20.firebaseio.com",
+//     projectId: "recipe-app-52f20",
+//     storageBucket: "recipe-app-52f20.appspot.com",
+//     messagingSenderId: "1016364414039"
+//   };
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
-// create database object
-const database = firebase.database();
-
-
-$.ajax({
-  type: 'GET',
-  url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=true&number=5&ingredients=apples%2Cflour%2Csugar%2Cmilk&limitLicense=true&ranking=2',
+// // create database object
+// const database = firebase.database();
 
 
-  // important part //
-  headers: {
-    'X-Mashape-Key': 'GlBSyGZxQXmshuRk2RcbiWgLs60sp1k5OFrjsnKS0C6LXsEpMr',
-    'X-Mashape-Host': 'spoonacular-recipe-food-nutrition-v1.p.mashape.com'
-  }
-  // important part //
+// $.ajax({
+//   type: 'GET',
+//   url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=true&number=5&ingredients=apples%2Cflour%2Csugar%2Cmilk&limitLicense=true&ranking=2',
 
 
-})
-.then(res => console.log(res))
-.catch(err => console.error(err));
-// .done(function(data) {
-//   alert(data);
+//   // important part //
+//   headers: {
+//     'X-Mashape-Key': 'GlBSyGZxQXmshuRk2RcbiWgLs60sp1k5OFrjsnKS0C6LXsEpMr',
+//     'X-Mashape-Host': 'spoonacular-recipe-food-nutrition-v1.p.mashape.com'
+//   }
+//   // important part //
+
+
 // })
+// .then(res => console.log(res))
+// .catch(err => console.error(err));
+// // .done(function(data) {
+// //   alert(data);
+// // })
 
 
 
@@ -82,7 +82,19 @@ function buildQueryURL() {
   // -H 'X-Mashape-Key: 4FEmgcoC9ZmshcPhJktTGdDBvHuEp1JalbejsnAA3CFG66uu08'
   // END API QUERY BUILDER SECTION --- END API QUERY BUILDER SECTION ---  
 
-  // LOGIC TO GET THE INGREDIENTS FROM THE DOM 
+  // LOGIC TO GET THE INPUTS FROM THE DOM 
+  
+  let category = [];
+  let postal = [];
+  let zipCode = $(this).attr("data-name");
+
+  $('#getAdventure').on("click", function (event) {
+    event.preventDefault();
+    let zipCode = $(".zipCode").val().trim();
+    postal.push(zipCode);
+    console.log(zipCode);
+  })
+
   
   // select all inputIngredients text fields with jQuery
   // and read the values into an array 
@@ -94,3 +106,5 @@ function buildQueryURL() {
   // service.textSearch(request, callback);
 
   // more detailed documentation for passing text based search requests https://developers.google.com/maps/documentation/javascript/places#TextSearchRequests
+
+});
