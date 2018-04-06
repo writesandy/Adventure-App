bebd6f2c3b27eadc2bd8185a8d21add40279e9fa
bvreeman <bvreeman@gmail.com>
2018-04-06 13:59:33 -0500

    made it so the museums pop up once the zipcode has been entered and get Adventure is pressed

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,10 +17,10 @@ $(document).ready(function() {
   let geoCodeKey="";
 
   firebase.initializeApp(config);
-  console.log("firebase database connection initialized");
+//   console.log("firebase database connection initialized");
   
   const database = firebase.database();
-  console.log("Local database variable assigned");
+//   console.log("Local database variable assigned");
 
 // Variables for bringin in long/lat from Google Geocoding API
 
@@ -38,7 +38,7 @@ $(document).ready(function() {
     // read the value of gck from the database
     database.ref().on("value", function(snapshot) {
       let geoCodeKey = snapshot.val().gck;
-      console.log("gck is " + geoCodeKey);
+    //   console.log("gck is " + geoCodeKey);
       return geoCodeKey;
     });
     let queryURL = "https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:" + zipCode + "&key=" + geoCodeKey;
@@ -49,25 +49,46 @@ $(document).ready(function() {
       method: "GET",
       dataType: "json",
       success: function(response){
-      console.log(response);
+    //   console.log(response);
             latitude = response.results[0].geometry.location.lat;
             longitude= response.results[0].geometry.location.lng;
-            console.log("Lat = "+latitude+"- Long = "+longitude);
+            // console.log("Lat = "+latitude+"- Long = "+longitude);
             returnZip = latitude + " " + longitude;
-            console.log("returnzip inside the func is " + returnZip);
+            // console.log("returnzip inside the func is " + returnZip);
             map.setCenter({lat: latitude, lng: longitude});
+            google.maps.event.addListener(map,'bounds_changed', function(event) {
+                // init nearbySearch
+                function callback(results, status) {
+                    if(status == google.maps.places.PlacesServiceStatus.OK){
+                        for (var i = 0; i < results.length; i++){
+                            markers.push(createMarker(results[i]));
+                        }
+                    }
+                }
+                //finds the type of place once the user location is determined
+                request = {
+                    location: map.getCenter(),
+                    radius: 6000,
+                    types: ['museum'],
+                };
+                // puts the locations on the map
+                service.nearbySearch(request, callback);           
+            })
       }
-      
     });
   }
 
-      $('#getAdventure').on("click", function (event) {
+    $('#pageSubmenu').on('click', function (event){
         event.preventDefault();
-        let zip = $(".zipCode").val().trim();
-        postal.push(zip);
-        console.log(zip);
-        getCoordinates(zip);
-      });
+    });
+
+    $('#getAdventure').on("click", function (event) {
+    event.preventDefault();
+    let zip = $(".zipCode").val().trim();
+    postal.push(zip);
+    // console.log(zip);
+    getCoordinates(zip);
+    });
       
     
     
@@ -78,116 +99,121 @@ $(document).ready(function() {
   // let eventQueryURL = `http://api.eventful.com/json/events/search?app_key${eventfulAPI}&q=${searchTerms}`;
   // };
 });
-  let map;
-  let infoWindow;
-  let request;
-  let service;
-  let markers = [];
-  let marker;
-  let pos;
-  let newRequest;
 
-  function initialize() {
-console.log('map loaded');
-      // definitions
-      map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 44.986656, lng: -93.258133},
-          zoom: 12,
-      });
-      infoWindow = new google.maps.InfoWindow();
-      service = new google.maps.places.PlacesService(map);
-
-      // init nearbySearch
-      function callback(results, status) {
-          if(status == google.maps.places.PlacesServiceStatus.OK){
-              for (var i = 0; i < results.length; i++){
-                  markers.push(createMarker(results[i]));
-              }
-          }
-      }
-      //finds the type of place once the user location is determined
-      request = {
-          location: map.getCenter(),
-          radius: 6000,
-          types: ['museum'],
-      };
-      // service.nearbySearch(request, callback);
-
-      // recenter map around user's location
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-              pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-              };
-              infoWindow.setPosition(pos);
-              map.setCenter(pos);
-
-              newRequest = {
-                  location: pos,
-                  radius: 5000,
-                  types: ['museum'],
-              };
-              service.nearbySearch(newRequest, callback);
-          }, function() {
-              handleLocationError(true, infoWindow, map.getCenter());
-          });
-      } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-          }
-
-      // recenter the map and reload the places after the map has been dragged and released
-      map.addListener('dragend', function() {
-          // google.maps.event.addListener(map, 'dragend', function(event) {
-
-          console.log(event);
-          map.getCenter()
-          console.log(map.getCenter());
-          clearResults(markers)
-          let request = {
-              location: map.getCenter(),
-              radius: 5000,
-              types: ['museum']
-          };
-          service.nearbySearch(request, callback);
-      })
-  }
-  // function callback(results, status) {
-  //     if(status == google.maps.places.PlacesServiceStatus.OK){
-  //         for (var i = 0; i < results.length; i++){
-  //             markers.push(createMarker(results[i]));
-  //         }
-  //     }
-  // }
+let map;
+let infoWindow;
+let request;
+let service;
+let markers = [];
+let marker;
+let pos;
+let newRequest;
+let callback;
 
 
+function initialize() {
+// console.log('map loaded');
+    // definitions
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 44.986656, lng: -93.258133},
+        zoom: 12,
+    });
+    infoWindow = new google.maps.InfoWindow();
+    service = new google.maps.places.PlacesService(map);
 
-  //creates a marker for the places
-  function createMarker(place) {
-      //customization of the icon happens at this line
-    //   let iconImage    = new google.maps.MarkerImage(place.icon, null, null, null, new google.maps.Size(32, 32));
-      const placeLoc = place.geometry.location;
-      marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-      });
-      //opens the infoWindow to show name and other information
-      google.maps.event.addDomListener(marker, 'click', function() {
-          infoWindow.setContent(place.name + '<br>' + place.id);
-          infoWindow.open(map, this);
-          console.log(place);
+    // init nearbySearch
+    function callback(results, status) {
+        if(status == google.maps.places.PlacesServiceStatus.OK){
+            for (var i = 0; i < results.length; i++){
+                markers.push(createMarker(results[i]));
+            }
+        }
+    }
+    //finds the type of place once the user location is determined
+    request = {
+        location: map.getCenter(),
+        radius: 6000,
+        types: ['museum'],
+    };
+    // service.nearbySearch(request, callback);
 
-      });
-      return marker;
-  }
-  //clears the markers when the map is moved so we don't keep leaving more and more markers
-  function clearResults(markers) {
-      for (let m in markers) {
-          markers[m].setMap(null)
-      }
-      markers = []
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
+    // recenter map around user's location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+            };
+            infoWindow.setPosition(pos);
+            map.setCenter(pos);
+
+            newRequest = {
+                location: pos,
+                radius: 5000,
+                types: ['museum'],
+            };
+            service.nearbySearch(newRequest, callback);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+    // recenter the map and reload the places after the map has been dragged and released
+    map.addListener('dragend', function() {
+        // google.maps.event.addListener(map, 'dragend', function(event) {
+    //   console.log(event);
+        map.getCenter()
+    //   console.log(map.getCenter());
+        clearResults(markers)
+        let request = {
+            location: map.getCenter(),
+            radius: 5000,
+            types: ['museum']
+        };
+        service.nearbySearch(request, callback);
+        return service.nearbySearch(request, callback);
+
+    })
+}
+// function callback(results, status) {
+//     if(status == google.maps.places.PlacesServiceStatus.OK){
+//         for (var i = 0; i < results.length; i++){
+//             markers.push(createMarker(results[i]));
+//         }
+//     }
+// }
+
+
+
+//creates a marker for the places
+function createMarker(place) {
+    //customization of the icon happens at this line
+//   let iconImage    = new google.maps.MarkerImage(place.icon, null, null, null, new google.maps.Size(32, 32));
+    const placeLoc = place.geometry.location;
+    marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+    //opens the infoWindow to show name and other information
+    google.maps.event.addDomListener(marker, 'click', function() {
+        infoWindow.setContent(place.name + '<br>' + place.id);
+        infoWindow.open(map, this);
+    //   console.log(place);
+
+    });
+    return marker;
+}
+//clears the markers when the map is moved so we don't keep leaving more and more markers
+function clearResults(markers) {
+    for (let m in markers) {
+        markers[m].setMap(null)
+    }
+    markers = []
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
